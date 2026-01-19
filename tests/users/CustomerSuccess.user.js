200bf72b31b56f1c27626147f4bb0a8b3568faae
Oluwatosin Ajao <ajaotj36@gmail.com>
2026-01-19 13:36:35 +0100

    refactor: migrate CustomerSuccess.user to Path A storageState

---
+++ tests/users/CustomerSuccess.user.js
@@ -0,0 +1,17 @@
+const { chromium } = require('@playwright/test');
+const path = require('path');
+
+module.exports = async function CustomerSuccess() {
+  const browser = await chromium.launch();
+  const context = await browser.newContext({
+    storageState: path.resolve(__dirname, '../../storage/admin.json'),
+  });
+
+  const page = await context.newPage();
+
+  return {
+    browser,
+    context,
+    page,
+  };
+};
